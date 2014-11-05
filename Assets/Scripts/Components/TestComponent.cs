d3e23af0a64ad77fdc8ff7d4643d1b69207a1fc8
Piotrus <kosmo.luki@gmail.com>
2014-11-05 16:00:30 +0100

    Initial. Framework.

---
+++ Assets/Scripts/Components/TestComponent.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestComponent : Framework.Core.SystemComponent {
+
+	public float speed;
+}
---
+++ Assets/Scripts/Systems/TestSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSystem : Framework.Core.System
+{
+	public override void OnUpdate ()
+	{
+		foreach (TestComponent tc in GetListOf<TestComponent>())
+		{
+			tc.transform.position += tc.speed * Vector3.up;
+		}
+	}
+}
