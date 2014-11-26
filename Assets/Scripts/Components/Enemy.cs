778d232a316ee43dce1cfab6a17cbe7aff4c92ea
Lukasz Kosma <kosmo.luki@gmail.com>
2014-11-26 21:46:09 +0100

    Dodanie potworka.

---
+++ Assets/Scripts/Components/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using Framework.Core;
+using UnityEngine;
+
+namespace Components
+{
+    public class Enemy : SystemComponent
+    {
+        public Rigidbody2D rigidbody;
+        public float speed;
+        public bool collisionWithObstacle;
+
+        public void Start()
+        {
+            rigidbody.velocity = new Vector2(-speed, 0.0f);
+        }
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Obstacles")
+            {
+                Debug.Log("asdsad");
+
+                foreach (ContactPoint2D point in collision.contacts)
+                {
+                    Vector2 vector = point.point - new Vector2(transform.position.x, transform.position.y);
+
+                    if (vector.x > 0.0f)
+                    {
+                        rigidbody.velocity = new Vector2(-speed, 0.0f);
+                    }
+                    else
+                    {
+                        rigidbody.velocity = new Vector2(speed, 0.0f);
+                    }
+                }
+            }
+            else if (collision.gameObject.tag == "Player")
+            {
+                foreach (ContactPoint2D point in collision.contacts)
+                {
+                    if (Vector2.Dot((point.point -
+                        new Vector2(transform.position.x, transform.position.y)).normalized,
+                        Vector2.up) > Mathf.Cos(Mathf.Deg2Rad * 45.0f))
+                    {
+                        GameObject.Destroy(gameObject);
+                    }
+                    else
+                    {
+                        GameObject.Destroy(collision.gameObject);
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/EnemySystem.cs
@@ -0,0 +1,21 @@
+﻿using Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Systems
+{
+    public class EnemySystem : Framework.Core.System
+    {
+        public override void OnFixedUpdate()
+        {
+            foreach (Enemy enemy in GetListOf<Enemy>())
+            {
+                Debug.Log(enemy.rigidbody.velocity);
+                enemy.rigidbody.velocity = enemy.speed * enemy.rigidbody.velocity.normalized;
+            }
+        }
+    }
+}
