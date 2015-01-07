3c8d4f9a81a58b4c8f484e83159c849c61a8373e
Lukasz Kosma <kosmo.luki@gmail.com>
2015-01-08 00:04:55 +0100

    Menu głowne i leveli.

---
+++ Assets/LevelMenuGUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelMenuGUI : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        // Make a background box
+        GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 90, 200, 180), "Level Menu");
+
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(Screen.width / 2 - 100 + 20, Screen.height / 2 - 55, 160, 60), "Level 1"))
+        {
+            Application.LoadLevel("scene1rock");
+        }
+
+        // Make the second button.
+        if (GUI.Button(new Rect(Screen.width / 2 - 100 + 20, Screen.height / 2 + 15, 160, 60), "Level 2"))
+        {
+            Application.LoadLevel("scene2");
+        }
+    }
+}
---
+++ Assets/MainMenuGUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        // Make a background box
+        GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 90, 200, 180), "Main Menu");
+
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(Screen.width / 2 - 100 + 20, Screen.height / 2 - 55, 160, 60), "Play"))
+        {
+            Application.LoadLevel("LevelMenu");
+        }
+
+        // Make the second button.
+        if (GUI.Button(new Rect(Screen.width / 2 - 100 + 20, Screen.height / 2 + 15, 160, 60), "Quit"))
+        {
+            Application.Quit();
+        }
+    }
+}
